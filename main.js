f72892d6b54fd59fd8c277d5c543ca7c32f49306
Azizfalshammari <azizfalshammari.com>
2024-05-26 20:55:40 +0300

    done

--- main.js
+++ main.js
@@ -28,13 +28,48 @@ console.log(ul2BDOM);
 let table = document.createElement('table');
 document.appendChild(table);
 
-let tr = document.createElement('tr')
-
-table.appendChild('tr')
 
 
+table.innerHTML(`
+<tr>
+<th></th>
+<th></th>
+<th></th>
+</tr>
+<tr>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+</table>
+`)
 
 
 
 
 
+
+
+
+
+
+function change()  {
+    let square1 = document.getElementsByClassName('square')[0];
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    square1.style.backgroundColor = '#' +randomColor;
+    
+    console.log(square1.style.top= Math.floor(Math.random() * 101) +'px')
+    console.log(square1.style.left= Math.floor(Math.random() * 101) +'px')
+    console.log('change is good')
+};
+setInterval(change, 3000);
